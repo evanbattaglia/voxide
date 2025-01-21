90e59c9ec3ff54f8e319592de6d5523a1d7f8b76
Evan Battaglia <ebattaglia@instructure.com>
2025-01-21 18:55:56 +0000

    Fix voxide-rails-routes.rb to not use self scripts
    Warning: still highly tailored to my setup...

--- scripts/voxide-rails-routes.rb
+++ scripts/voxide-rails-routes.rb
@@ -1,7 +1,45 @@
 #!/usr/bin/env ruby
+require 'shellwords'
+require 'tempfile'
+
+# This script:
+# 1. Extracts Rails routes from the Rails app using `rails runner`
+#      assumes rails container running in "web" under docker compose!
+# 2. Saves the routes in a cache file so we don't have to run `rails runner` every time
+# 3. Presents the routes in fzf
+# 4. Opens the controller file in the editor at the action definition
+
 Dir.chdir `git rev-parse --show-toplevel`.chomp
 
-cmd = "rails-routes-cached | fzf -n 1,2 --with-nth 1,2 --multi"
+# Service name in docker-compose.yml we use to get routes from Rails (rails runner)
+DOCKER_COMPOSE_CONTAINER_NAME="web"
+
+def ensure_rails_routes_cache_loaded(cache_file)
+  return if File.exist?(cache_file)
+  Tempfile.open("voxide-rails-routes") do |f|
+    f.write <<~RUBY
+      Rails.application.routes.routes.each do |route|
+        # Get the HTTP verb and path
+        http_method = route.verb
+        path = route.path.spec.to_s
+
+        # Get the controller and action (optional)
+        requirements = route.defaults
+        controller = requirements[:controller]
+        action = requirements[:action]
+
+        puts [http_method, path, controller, action].join("\t")
+      end
+    RUBY
+    f.flush
+    system "cat #{f.path.shellescape} | docker compose exec -T #{DOCKER_COMPOSE_CONTAINER_NAME} rails runner - 2> >(grep -v ^Running.via.Spring >&2) > #{cache_file.shellescape}"
+  end
+end
+
+git_repo_dir = `git rev-parse --show-toplevel`.chomp
+cache_file = File.join(git_repo_dir, ".voxide-rails-routes")
+ensure_rails_routes_cache_loaded(cache_file)
+cmd = "fzf -n 1,2 --with-nth 1,2 --multi < #{cache_file.shellescape}"
 if initial_search = ARGV.first
   require "shellwords"
   cmd += " --query #{initial_search.shellescape}"
